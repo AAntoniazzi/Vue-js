a3f7faa15ffff91b64f1729683e9e1c8911c438f
AAntoniazzi <aramys.antoniazzi@gmail.com>
2020-10-19 18:18:20 -0300

    Vue.JS - Add item

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="App">
+        <div>My app</div>
+        <BuyList
+            listName="Aramys"
+            :products="localProducts"
+            @add-item="AddItemApp"
+        />
+        <BuyList
+            listName="Giordano"
+            :products="localProducts2"
+        />
+    </div>
+</template>
+
+<script>
+    import BuyList from './BuyList.vue';
+
+    export default {
+        components: {
+            BuyList,
+        },
+
+        props: {},
+        
+        data() {
+            return {
+                localProducts: [
+                    { id: 1, name: 'Tomate cereja' },
+                    { id: 2, name: 'Cebola' },
+                ],
+                localProducts2: [
+                    { id: 3, name: 'Alho poró' },
+                    { id: 4, name: 'Sardinha' },
+                ],
+            };
+        },
+
+        methods: {
+            AddItemApp(item) {
+                this.localProducts.push({ id: +new Date(), name: item });
+            },
+        },
+    };
+</script>
---
+++ BuyList.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="BuyList">
+        <span>Buy List {{listName}}</span>
+        <div>
+            <ul>
+                <li
+                    v-for="item in filteredList"
+                    :key="item.id"
+                    >{{item.name}}
+                </li>
+            </ul>
+            <input v-model="itemName" />
+            <button @click="addItem">Add item</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            listName: {
+                type: String,
+                default: '',
+            },
+            products: {
+                type: Array,
+                default: () => [],
+            },
+        },
+
+        data() {
+            return {
+                itemName: '',
+            };
+        },
+
+        mounted() {},
+
+        methods: {
+            filterFeijao() {},
+
+            addItem() {
+                this.$emit('add-item', this.itemName);
+                this.itemName = '';
+            },
+        },
+
+        computed: {
+            filteredList() {
+                return this.products;
+            },
+        },
+
+        watch: {},
+    }
+</script>
+
+<style>
+    .BuyList {
+        padding: 20px;
+        border: 1px solid #ddd;
+        margin-bottom: 20px;
+    }
+
+    .BuyList ul li {
+        border-bottom: 1px solid #ddd;
+        margin-bottom: 20px;
+    }
+</style>
